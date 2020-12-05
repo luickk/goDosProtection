0eaab0991c5f65acc2f21578b3b7347eea325822
Luick Klippel <luickklippel@Luicks-MacBook-Pro.local>
2020-12-05 21:39:08 +0100

    adding code foundation

---
+++ pkg/goDosProtection/goDosProtection.go
@@ -0,0 +1,47 @@
+package goDosProtection
+
+import (
+  "time"
+)
+
+type timerRoutine struct {
+  timer time.Timer
+  expired bool
+}
+
+type dosController struct {
+  ClientMap map[string]*timerRoutine
+  ReconnectTimeLimit int
+}
+
+
+func New(reconnectTimeLimit int) dosController{
+  return dosController { make(map[string]*timerRoutine), reconnectTimeLimit }
+}
+
+// checks wether client's time is expired
+// returns true if time is expired, false if it isn't
+// registers new client if not in map
+func (dC *dosController) Client(address string) bool {
+  if client, ok := dC.ClientMap[address]; !ok {
+    dC.ClientMap[address] = TimerRoutine(time.Second * 1)
+    return true
+  } else if client.expired {
+    return true
+  }
+  return false
+}
+
+func (dC *dosController) LookUpClient(address string) {
+  // if dC.ClientMap[address].
+}
+
+func TimerRoutine(expTime time.Duration) *timerRoutine {
+  timer := time.NewTimer(expTime)
+  tR := timerRoutine { *timer, false }
+  go func(){
+      <-timer.C
+      tR.expired = true
+  }()
+  return &tR
+}
---
+++ test/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "goDosProtection/pkg/goDosProtection"
+)
+
+func main() {
+  goDosProtection.New(5)
+}
